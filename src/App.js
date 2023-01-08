3130f223377b5a75cdb7c77598759301d6caf85c
Jung Gyu Yoon <yjg30737@gmail.com>
2023-01-08 18:03:32 +0900

    Header, content, footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+const Layout = ({ header, children, footer }) => (
+  <div className="min-h-screen flex flex-col">
+    <header className="bg-blue-500 p-4 shadow-md">
+      <h1 className="text-2xl font-bold text-white">{header}</h1>
+    </header>
+    <main className="flex-1 p-4 flex items-center justify-center text-3xl">
+      {children}
+    </main>
+    <footer className="bg-blue-400 p-4 text-center text-white">
+      {footer}
+    </footer>
+  </div>
+);
 
 function App() {
   return (
-    <div>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Click me
-        </button>
+    <div className="App">
+        <Layout header="YJG30737's Tailwind CSS tutorial with React" footer="Powered by React & Tailwind CSS">
+          <p>This is the main content of the app.</p>
+        </Layout>
     </div>
   );
 }
